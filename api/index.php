4606845774fc60cad6202850b0d3424edcd81059
bikilaketema <bikilaketema94@gmail.com>
2025-08-14 22:10:25 +0300

    feat: integrate PHPMailer for email functionality and add dotenv support for environment variables

--- api/index.php
+++ api/index.php
@@ -1,10 +1,15 @@
 <?php
+
+require_once __DIR__ . '/vendor/autoload.php';
 // Set default headers
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
 // Include all controller files
 require_once __DIR__ . '/src/modules/auth/auth-controller.php';
@@ -14,9 +19,9 @@ require_once __DIR__ . '/src/modules/lookups/lookup-controller.php';
 // Add other controllers as you create them...
 
 use src\modules\auth\AuthController;
-use src\modules\user\UserController;
 use src\modules\lookups\LookupController;
 use src\modules\customers\CustomerController;
+use src\modules\user\UserController;
 
 // --- Basic Routing ---
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
@@ -32,7 +37,7 @@ switch ($module) {
     case 'auth':
         $controller = new AuthController();
         break;
-    case 'user':
+    case 'users':
         $controller = new UserController();
         break;
     case 'lookups':
