0a659e3249747e756b70698f866431d8d48d0a80
biniyam-29 <bnmmarkos@gmail.com>
2025-08-19 22:25:32 +0300

    fix: backend cors issue

--- api/index.php
+++ api/index.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 // CORS headers
-header("Access-Control-Allow-Origin: http://localhost:5173"); // allow your frontend
+header("Access-Control-Allow-Origin: *"); 
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
