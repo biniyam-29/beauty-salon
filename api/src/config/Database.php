d34e4226a1f861164f9a7c4dec433f2cc3ff666d
biniyam-29 <bnmmarkos@gmail.com>
2025-12-17 13:24:37 +0300

    db credentials

--- api/src/config/Database.php
+++ api/src/config/Database.php
@@ -11,8 +11,8 @@ use PDOException;
 class Database {
     // --- Database Credentials ---
     private static string $host = 'localhost';
-    private static string $db_name = 'beauty_db';
-    private static string $username = 'beauty'; // UPDATED: Using the new, more secure user
+    private static string $db_name = 'in2skicn_beauty_db';
+    private static string $username = 'in2skicn_beauty'; // UPDATED: Using the new, more secure user
     private static string $password = 'passxsaqwer4321'; 
 
     // Holds the single instance of the database connection
